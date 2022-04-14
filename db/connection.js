344291a6b3e8d6bac983c855de37dcab5796f278
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-04-14 11:44:38 -0400

    add connection.js code and make route files

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      //MySQL password
+      password: 'hazelnut',
+      //Database related to this program
+      database: 'workforce'
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./deptRoutes'));
+router.use(require('./roleRoutes'));
+router.use(require('./employeeRoutes'));
+
+module.exports = router;
