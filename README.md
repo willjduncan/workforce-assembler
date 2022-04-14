3f3a21042503b98aa6fe2da5df8f267aaea5ccc3
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-04-14 15:08:01 -0400

    add GET POST and DELETE methods for roles

--- README.md
+++ README.md
@@ -72,7 +72,7 @@ View employees by manager.
 
 View employees by department.
 
-Delete departments, roles, and employees.
+Delete roles, and employees.
 
 View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
 
--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -1,3 +1,59 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
+const cTable = require('console.table');
+
+// View all roles in a table
+router.get('/roles', (req, res) => {
+    const sql = `SELECT * FROM roles`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json(console.table(rows))
+      });
+});
+
+
+// Create a new role
+router.post('/roles', ({ body }, res) => {
+    const sql = `INSERT INTO roles (title, salary, department_id)
+    VALUES (?,?,?)`;
+    const params = [body.title, body.salary, body.department_id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+
+// Delete roles
+router.delete('/roles/:id', (req, res) => {
+    const sql = `DELETE FROM roles WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        // checks if anything was deleted
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'role not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+});
+
+module.exports = router;
